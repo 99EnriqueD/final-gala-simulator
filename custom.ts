85fd9b012d88eadacac271947a8cd031ccbbbc7c
TheInventor007 <81957063+TheInventor007@users.noreply.github.com>
2021-04-29 16:27:56 +0200

    Update pxt.json, custom.ts

--- custom.ts
+++ custom.ts
@@ -7,34 +7,36 @@
  * Custom radio blocks
  */
 //% weight=100 color=#FF1493
-namespace radio {
-    
+namespace IoTeach {
     /**
-     * Transmits a value respresenting the propellers voltage over the radio.
-     * 
-     * @param team_name the name of the team which is sending the propeller value
-     * @param value the propeller value
-     */
-    //% block="radio send propeller voltage $value $team_name"
-    export function radio_propeller (team_name: string, value: number) {
-        radio.sendValue("" + team_name + "." + "propeller", value)
+    * niks
+    */
+    //% block
+    export function Blazen () {
+    let datapunt
+    datapunt = 0
+    basic.pause(100 / diameter_bronchioli_mm)
+    serial.writeValue(Persoon, datapunt)
+    for (let index = 0; index <= 7; index++) {
+        datapunt = datapunt + longcapaciteit_L * diameter_bronchioli_mm / (index + 1)
+        basic.pause(100 / diameter_bronchioli_mm)
+        serial.writeValue(Persoon, datapunt)
     }
-
-    /**
-     * Transmits a spike value respresenting an fev1 over the radio.
-     * 
-     * This function prevents ugly interpolation in the built-in simulator.
-     * 
-     * @param team_name the name of the team which is sending the fev1 value
-     * @param fev1 the fev1 value
-     */
-    //% block="radio send fev1 $fev1 $team_name"
-    export function radio_fev1 (team_name: string, fev1: number) {
-        radio.sendValue("" + team_name + "." + "fev1", 0)
-        basic.pause(30)
-        radio.sendValue("" + team_name + "." + "fev1", fev1)
-        basic.pause(30)
-        radio.sendValue("" + team_name + "." + "fev1", 0)
+    for (let index = 0; index < longcapaciteit_L / 2; index++) {
+        datapunt += longcapaciteit_L * diameter_bronchioli_mm / 40
+        basic.pause(100 / diameter_bronchioli_mm)
+        serial.writeValue(Persoon, datapunt)
     }
-
+    for (let index = 0; index < longcapaciteit_L / 2; index++) {
+        datapunt += longcapaciteit_L * diameter_bronchioli_mm / -40
+        basic.pause(100 / diameter_bronchioli_mm)
+        serial.writeValue(Persoon, datapunt)
+    }
+    while (datapunt > 1.5) {
+        datapunt = datapunt - datapunt / 10
+        serial.writeValue(Persoon, datapunt)
+        basic.pause(100)
+    }
+    basic.pause(500)
+};
 }
