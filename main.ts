ac42022bf9fc2011741a5ed6fb379de050f77706
TheInventor007 <81957063+TheInventor007@users.noreply.github.com>
2021-04-29 16:26:27 +0200

    Update pxt.json, main.ts, tutorial.md, Custom.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
-basic.forever(function () {
-	
+let Persoon = ""
+let longcapaciteit_L = 0
+let diameter_bronchioli_mm = 0
+function Controle () {
+    Persoon = "persoon.Controle"
+    longcapaciteit_L = 6
+    diameter_bronchioli_mm = 1
+    IoTeach.Blazen()
+}
+input.onButtonPressed(Button.A, function () {
+    Controle()
+    Astma()
 })
+function Astma () {
+    Persoon = "persoon.Asthma"
+    longcapaciteit_L = 8
+    diameter_bronchioli_mm = 1.3
+    IoTeach.Blazen()
+}
